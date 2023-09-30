fe89b22b2b61f8e17c958b390cfb9285115e4e3c
mbpye <mbpye2361@gmail.com>
2023-09-30 12:00:37 +0300

    30.09.2023 update

--- offsets.cs
+++ offsets.cs
@@ -1,7 +1,7 @@
 public static class client_dll {
-    public const nint entityList = 0x178C878;
-    public const nint globalVars = 0x168FCC8;
-    public const nint localPlayerController = 0x17DB0F8;
-    public const nint viewAngles = 0x18D9590;
-    public const nint viewMatrix = 0x187A6D0;
+    public const nint entityList = 0x178C8A8;
+    public const nint globalVars = 0x168FCE8;
+    public const nint localPlayerController = 0x17DB128;
+    public const nint viewAngles = 0x18D95C0;
+    public const nint viewMatrix = 0x187A700;
 }
--- offsets.hpp
+++ offsets.hpp
@@ -3,9 +3,9 @@
 #include <cstddef>
 
 namespace client_dll {
-    constexpr std::ptrdiff_t entityList = 0x178C878;
-    constexpr std::ptrdiff_t globalVars = 0x168FCC8;
-    constexpr std::ptrdiff_t localPlayerController = 0x17DB0F8;
-    constexpr std::ptrdiff_t viewAngles = 0x18D9590;
-    constexpr std::ptrdiff_t viewMatrix = 0x187A6D0;
+    constexpr std::ptrdiff_t entityList = 0x178C8A8;
+    constexpr std::ptrdiff_t globalVars = 0x168FCE8;
+    constexpr std::ptrdiff_t localPlayerController = 0x17DB128;
+    constexpr std::ptrdiff_t viewAngles = 0x18D95C0;
+    constexpr std::ptrdiff_t viewMatrix = 0x187A700;
 }
